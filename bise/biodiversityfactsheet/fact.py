338c6eb7b26c4267d41c8b3b547f92d4a17dc4ee
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-07-05 10:26:27 +0200

    subscriber to hide from navigation

--- bise/biodiversityfactsheet/fact.py
+++ bise/biodiversityfactsheet/fact.py
@@ -1,24 +1,8 @@
+from zope.app.container.interfaces import IObjectAddedEvent
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from bise.biodiversityfactsheet import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -26,7 +10,6 @@ class IFact(form.Schema, IImageScaleTraversable):
     """
     Factsheet content
     """
-
 
 
 # Custom content-type class; objects created for this content type will
@@ -54,3 +37,8 @@ class FactView(grok.View):
     grok.context(IFact)
     grok.require('zope2.View')
     grok.name('view')
+
+
+@grok.subscribe(IFact, IObjectAddedEvent)
+def exclude_from_nav(context, event):
+    context.exclude_from_nav = True
--- bise/biodiversityfactsheet/section.py
+++ bise/biodiversityfactsheet/section.py
@@ -1,24 +1,9 @@
+from zope.app.container.interfaces import IObjectAddedEvent
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from bise.biodiversityfactsheet import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -37,4 +22,6 @@ class Section(dexterity.Container):
     # Add your class methods and properties here
 
 
-
+@grok.subscribe(ISection, IObjectAddedEvent)
+def exclude_from_nav(context, event):
+    context.exclude_from_nav = True
