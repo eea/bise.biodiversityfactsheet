208515d802ac293886f71f9dfea195b7d0942dcd
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-07-05 11:58:10 +0200

    fact view templates

--- bise/biodiversityfactsheet/fact.py
+++ bise/biodiversityfactsheet/fact.py
@@ -1,8 +1,25 @@
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
+from plone.app.contentlisting.interfaces import IContentListing
 from zope.app.container.interfaces import IObjectAddedEvent
 from five import grok
 from plone.directives import dexterity, form
-
 from plone.namedfile.interfaces import IImageScaleTraversable
+from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from plone.app.textfield import RichText
+
+from bise.biodiversityfactsheet import MessageFactory as _
+
+
+fact_icons = SimpleVocabulary(
+    [
+     SimpleTerm(value=u'text', title=_(u'Text')),
+     SimpleTerm(value=u'map', title=_(u'Icon')),
+     SimpleTerm(value=u'download', title=_(u'Download')),
+     SimpleTerm(value=u'graph', title=_(u'Graph')),
+     ]
+    )
 
 
 # Interface class; used to define content-type schema.
@@ -10,6 +27,27 @@ class IFact(form.Schema, IImageScaleTraversable):
     """
     Factsheet content
     """
+
+    text = RichText(
+        title=_(u'Text'),
+        description=_(u'Text of this fact'),
+        required=False,
+        )
+
+    embed = schema.Text(
+        title=_(u'Embed code'),
+        description=_(u'Paste here content coming from external site. '
+                      u'Such as iframe, object, or other external site code.'
+            ),
+        required=False,
+        )
+
+    fact_icon = schema.Choice(
+        title=_(u'Icon to show next to the title'),
+        vocabulary=fact_icons,
+        default=u'text',
+        required=True
+        )
 
 
 # Custom content-type class; objects created for this content type will
@@ -29,7 +67,6 @@ class Fact(dexterity.Container):
 # interface with "/@@view" appended unless specified otherwise
 # using grok.name below.
 # This will make this view the default view for your content-type
-
 grok.templatedir('templates')
 
 
@@ -37,6 +74,29 @@ class FactView(grok.View):
     grok.context(IFact)
     grok.require('zope2.View')
     grok.name('view')
+
+    def contents(self):
+        context = aq_inner(self.context)
+        contents = getMultiAdapter((context, self.request),
+            name=u'factrenderview')()
+
+        return contents
+
+
+class FactRenderView(grok.View):
+    grok.context(IFact)
+    grok.require('zope2.View')
+    grok.name('factrenderview')
+
+    def files(self):
+        context = aq_inner(self.context)
+        files = context.getFolderContents({'portal_type': 'File'})
+        return IContentListing(files)
+
+    def links(self):
+        context = aq_inner(self.context)
+        links = context.getFolderContents({'portal_type': 'Link'})
+        return IContentListing(links)
 
 
 @grok.subscribe(IFact, IObjectAddedEvent)
