b92e41e7d662457535a2fcc886a354e0fe831d3e
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-07-05 10:31:17 +0200

    memoize view

--- bise/biodiversityfactsheet/biodiversityfactsheet.py
+++ bise/biodiversityfactsheet/biodiversityfactsheet.py
@@ -3,25 +3,10 @@ from plone.app.contentlisting.interfaces import IContentListing
 from plone.app.contentlisting.interfaces import IContentListingObject
 from five import grok
 from plone.directives import dexterity, form
+from plone.memoize.view import memoize
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
-
-from plone.app.textfield import RichText
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
-from bise.biodiversityfactsheet import MessageFactory as _
 
 
 # Interface class; used to define content-type schema.
@@ -57,6 +42,7 @@ class BiodiversityFactsheetView(grok.View):
     grok.require('zope2.View')
     grok.name('view')
 
+    @memoize
     def facts(self):
         context = aq_inner(self.context)
         sections = context.getFolderContents({'portal_type': 'Section'})
