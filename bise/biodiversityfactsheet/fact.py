b930e99cc663eb9727aee933c71960cf3fd73268
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-07-05 09:24:03 +0200

    sections are composed by facts

---
+++ bise/biodiversityfactsheet/fact.py
@@ -0,0 +1,56 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from bise.biodiversityfactsheet import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+class IFact(form.Schema, IImageScaleTraversable):
+    """
+    Factsheet content
+    """
+
+
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+class Fact(dexterity.Container):
+    grok.implements(IFact)
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# templates called factview.pt .
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@view" appended unless specified otherwise
+# using grok.name below.
+# This will make this view the default view for your content-type
+
+grok.templatedir('templates')
+
+
+class FactView(grok.View):
+    grok.context(IFact)
+    grok.require('zope2.View')
+    grok.name('view')
