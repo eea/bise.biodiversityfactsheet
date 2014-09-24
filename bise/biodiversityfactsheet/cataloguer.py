d16cc84f7fd5791c717f033c1777eec86f32b4d6
MikelS <mikel.santamaria@gmail.com>
2014-09-24 11:06:38 +0200

    Factsheet tagging support

--- bise/biodiversityfactsheet/cataloguer.py
+++ bise/biodiversityfactsheet/cataloguer.py
@@ -17,5 +17,18 @@ class FactsheetCataloguer(PACDocumentCataloguer):
                     name='factrenderview')()
 
         items['article[content]'] = contents
+        targets = []
+        for target in self.context.targets:
+            targets.append(target)
+        items['article[target_list]'] = u','.join(targets)
+        actions = []
+        for action in self.context.actions:
+            actions.append(action)
+        items['article[action_list]'] = u','.join(actions)
+        tags = []
+        for tag in self.context.cataloguetags:
+            tagid, tagname = tag.split('-')
+            tags.append(tagname)
+        items['article[tag_list]'] = u','.join(tags)        
 
         return items
