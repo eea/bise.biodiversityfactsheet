cdebfa83d6208a4587c8710e3d33c260fcd36a3e
MikelS <mikel.santamaria@gmail.com>
2014-12-01 13:08:48 +0100

    Allow fact filtering by associated target(s)

--- bise/biodiversityfactsheet/biodiversityfactsheet.py
+++ bise/biodiversityfactsheet/biodiversityfactsheet.py
@@ -63,12 +63,26 @@ class BiodiversityFactsheetView(grok.View):
         context = aq_inner(self.context)
         sections = context.getFolderContents({'portal_type': 'Section'})
         fact_data = []
+
+        targets = self.request.form.get('targets', '')
+
         for section in sections:
             data = {}
             data['object'] = IContentListingObject(section)
             section_object = data['object'].getObject()
             facts = section_object.getFolderContents({'portal_type': 'Fact'})
-            data['facts'] = IContentListing(facts)
-            fact_data.append(data)
+            fact_list = []
+            if targets == "":
+                fact_list = facts
+            else:
+                for fact in facts:
+                    targetsArray = targets.split(",")
+                    if fact.getObject().targets is not None:
+                        for target in targetsArray:
+                            if target in fact.getObject().targets:
+                                fact_list.append(fact)
+            if len(fact_list) > 0 or section_object.Title() == "General information":
+                data['facts'] = IContentListing(fact_list)
+                fact_data.append(data)
 
         return fact_data
